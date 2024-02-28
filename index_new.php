349df87ff8348b61ec22c047d2739ec9bdfaeb7d
Donald Peters <donny9585@gmail.com>
2024-02-28 14:03:29 +0100

    Fixed PHP script with LitCal API

--- index_new.php
+++ index_new.php
@@ -1,13 +1,30 @@
 <?php
 
+function dateToUnixTime() {
+    $date = new DateTime(); // Create a DateTime object representing the current date and time
+    $date->setTime(0, 0, 0); // Set the time components to midnight (0 hours, 0 minutes, 0 seconds)
+    $timestamp = $date->getTimestamp(); // Get the Unix timestamp
 
-$url = 'https://litcal.johnromanodorazio.com/api/v3/LitCalEngine.php?year=2024&epiphany=SUNDAY_JAN2_JAN8&ascension=SUNDAY&corpusChristi=SUNDAY&locale=en_NG&returntype=JSON';
+    return $timestamp + 3600; // Add 3600 seconds (1 hour) to the Unix timestamp
+}
+
+function getCurrentYear($timestamp) {
+    $date = new DateTime("@$timestamp"); // Create a DateTime object from the Unix timestamp
+    // Extract the year component
+
+    return $date->format('Y');
+}
+
+// Get the current year
+$timestamp = dateToUnixTime(); // Get the modified Unix timestamp
 
 function getData()
 {
-    global $url; // Use global variable $url inside the function
+    global $timestamp;
+    $currentYear = getCurrentYear($timestamp);
     try {
-        $response = file_get_contents($url); // Fetch data from URL
+        // Fetch data from URL
+        $response = file_get_contents(sprintf('https://litcal.johnromanodorazio.com/api/v3/LitCalEngine.php?year=%s&epiphany=SUNDAY_JAN2_JAN8&ascension=SUNDAY&corpusChristi=SUNDAY&locale=en_NG&returntype=JSON', $currentYear));
         if ($response === false) {
             throw new Exception('Failed to fetch data from URL');
         }
@@ -19,22 +36,23 @@ function getData()
 }
 
 $data = getData(); // Call getData() function to fetch data
-//$season = '';
-//$date = '';
-//$color = '';
-//$title = '';
-// Insert DOM manipulation here (or any other processing)
+$todaysLiturgy = [];
+
 if ($data !== false) {
-    echo "<pre>";
-//    $season = $data['season'];
-//    $date = $data['date'];
-//    $color = $data['celebrations'][0]['colour'];
-//    $title = $data['celebrations'][0]['title'];
-    var_dump($data['LitCal']);
-    echo "</pre>";
+    $data = $data['LitCal'];
+    foreach ($data as $liturgyDay) {
+        if ($liturgyDay['date'] == $timestamp) {
+            $todaysLiturgy[] = $liturgyDay;
+        }
+    }
 } else {
     echo "Error fetching data";
 }
 
-echo "<h1>Hello WordPress</h1>";
+$color = $todaysLiturgy[0]['color'][0];
+$title = $todaysLiturgy[0]['name'];
+$date = new DateTime("@$timestamp");
+$date = $date->format('Y-m-d');
 
+echo "<h2>$title" . " [" . $date . "]" . "</h2>";
+echo "<div style='background: $color'>Vestment Color: $color</div>";
